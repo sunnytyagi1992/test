94724613f7f9ad7d6bc30ceefa062c348ed50136
sunnytyagi1992 <sunnyinfopedia@gmail.com>
2023-10-19 06:03:40 +0530

    not completed

---
+++ BmiCalculator/bmiCalculator.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+})
+
+// bmiCalculator =() => {
+//     results = height+weight;
+//     console.log(results);
+// }
